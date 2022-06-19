904db3f04154d2556508d1cb68e7b3662c34664e
Alex <alexander.h.perry@gmail.com>
2022-06-19 20:39:10 +1000

    LocalStorage functions completed. Timer Readded. Onclick function adjusted to display 'saved' for 3 seconds on click.

--- assets/script.js
+++ assets/script.js
@@ -1,90 +1,64 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+// JavaScript file for work-day-scheduler
 
-// <p id="currentDay" class="lead"></p> <---- Current Date
-// </header>
-// <div class="container">
-//   <!-- Timeblocks go here -->
-// </div>
+var currentHour = moment().hour(); //reused in timer + updateCalendar function
 
-//using 
-// * bootstrap
-// * jquery
-// * fontawesome - the icons
-// * moment
-var currentDay = moment().format("dddd, MMMM Do");
-var currentHour = moment().hour();
-$("#currentDay").text(currentDay);
-
-
-//TODO:
-// 3 columns, timeslot, the event, save button
-// event is text entry that can be modded, save button saves to local storage.
-// colour code the events based on time.
-
-
-// 1 minute timer to refresh current day/hour and change the classes based on past/present/future. -- No need to run the check any more frequent than this.
-
-//Move below into a function... 
-
-setInterval(() => {
-    currentDay = moment().format("dddd, MMMM Do");
+// Function to update the work-day calendar:
+// * Colour code each row based on whether past, present or future.
+// * Load any existing event entries from local storage
+// * pageLoad argument: Boolean value to only load from local storage on page-load. 
+function updateCalendar(pageLoad) {
+    var currentDay = moment().format("dddd, MMMM Do");
     currentHour = moment().hour();
-    if($("#currentDay").text() != currentDay){
-        $("#currentDay").text(currentDay);
-    };
-    console.log($("#currentDay").text());
-
+    $("#currentDay").text(currentDay); // display the current day
+    // Loop through each row, set class based on current time vs row time. 
     $(".row").each(function (index) {
         //Check for Past
-        if (parseInt($(this).attr("data-hour")) < currentHour && !$(this).hasClass("past")) {               
+        if (parseInt($(this).attr("data-hour")) < currentHour && !$(this).hasClass("past")) {
             $(this).removeClass("present future").addClass("past");
-            console.log("past added");
         };
         //Check for Present
-        if (parseInt($(this).attr("data-hour")) == currentHour && !$(this).hasClass("present")) {               
+        if (parseInt($(this).attr("data-hour")) == currentHour && !$(this).hasClass("present")) {
             $(this).removeClass("past future").addClass("present");
-            console.log("present added");
         };
         //Check for Future
-        if (parseInt($(this).attr("data-hour")) > currentHour && !$(this).hasClass("future")) {               
+        if (parseInt($(this).attr("data-hour")) > currentHour && !$(this).hasClass("future")) {
             $(this).removeClass("past present").addClass("future");
-            console.log("future added");
         };
-        // if localStorage.getItem($(this).attr("id")    )
-        console.log($(this).attr("id"));
-        var currentID = $(this).attr("id");  
 
-        if (localStorage.getItem(currentID) !== null) {
-            console.log(localStorage.getItem(currentID));
-            
-            // $(this).children(".description").val()
-        }
-
+        // Update events from local storage when function is called with pageLoad flagged as true only.
+        if (pageLoad) {
+            var storageItem = localStorage.getItem($(this).attr("id"));
+            if (storageItem !== null) { // only update the field if there is an entry in the local storage. 
+                $(this).children(".description").val(storageItem);
+            }
+        };
     });
-    
-}, 10000);
+}
 
-$(".saveBtn").click(function(event){
+// 1 minute timer to check the current hour. If hour has changed since previous updateCalendar function call, run it again to apply colour coding
+// Does not trigger load from localstorage. 
+// NOTE: There is a delay (2-3 mins) if time is adjusted via system settings
+setInterval(() => {
+    var tempHour = moment().hour();
+    if (currentHour != tempHour) {
+        console.log("Hour Changed");
+        updateCalendar(0);
+    }
+}, 60000);
+
+// Save to local storage when 'Save' button is clicked for the item. 
+$(".saveBtn").click(function (event) {
     event.stopPropagation();
-    //save to local storage
-    var elementID = $(this).parent().attr("id"); //get the ID of the row
+    var elementID = $(this).parent().attr("id"); //get the ID of the row clicked. 
     var elementText = $(this).siblings("textarea").val(); // get the text in the textarea
-
     localStorage.setItem(elementID, elementText);
 
+    // Display 'Saved' for 3 seconds, then change back to the saved icon. 
+    $(this).text("Saved");
+    setTimeout(() => {
+        $(this).html("<i class='fas fa-save'></i>");
+    }, 3000);
 });
 
-// click event for save button. That's really it... trigger event off 'this' row. 
-// this.siblings(textarea)
+// Run updateCalendar function on page load. 
+updateCalendar(1);
